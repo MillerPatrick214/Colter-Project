fff127bf559f6850d5576714c72be108ab502f35
MillerPatrick214 <149214352+MillerPatrick214@users.noreply.github.com>
2024-10-06 18:32:59 -0700

    FPS counter/Sprite Vector math update
    Added an FPS counter
    finished the vector math node that will handle drawing the correct sprite face when needed.

---
+++ UI/FPS.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class FPS : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	Label labelNode;
+	public override void _Ready()
+	{
+		labelNode = GetNodeOrNull<Label>("Label");
+		if (labelNode == null) {
+			GD.Print("FPS Node: returned null for child node");
+		}
+
+		else {
+			GD.Print("Ladies and gentlemen... We got him...");
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		labelNode.Text = $"FPS: {Engine.GetFramesPerSecond()}";
+	}
+}
