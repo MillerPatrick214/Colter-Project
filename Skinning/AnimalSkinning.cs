b576c17ef26a9cbcc27fca70e5c20352d9069017
MillerPatrick214 <149214352+MillerPatrick214@users.noreply.github.com>
2025-01-19 20:42:46 -0800

    Skinning activity quality ratings implemented
    Needs work but we're basically there. Just having some issues with the timer. Next stop, inventory?

--- Skinning/AnimalSkinning.cs
+++ Skinning/AnimalSkinning.cs
@@ -1,7 +1,7 @@
 using Godot;
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 
 public partial class AnimalSkinning : Control
 { 
@@ -22,23 +22,44 @@ public partial class AnimalSkinning : Control
 	Skinnable currSkinnable;
 	TextureRect Sheathe;
 
+	RichTextLabel SkinComment;
+
 	Line2D CutLine;
+	Timer timer;
 	
-	float totalLength;
+	float MaxLength;
 	float SegmentLength; 
 
 	int LineIndex;
+	float devAccum;
+
 	Godot.Vector2 DefaultBowiePosition;
 	Godot.Vector2 DefaultSheathePosition;
 
+	Dictionary<int, string> DictRating = new Dictionary<int, string> 
+	{
+		{800, "Perfect!"},
+		{1750, "Good!"},
+		{2500, "Decent"},
+		{3250, "Poor..."},
+		{4000, "Shite!"}
+	}; 
+
+	
 	public override void _Ready()
 	{
 		BowieKnife = GetNodeOrNull<TextureRect>("BowieKnife");
 		KnifeAreaNode = GetNodeOrNull<KnifeArea>("BowieKnife/Knife Area");
 		skinningfact = GetNodeOrNull<SkinningFactory>("Skinning Factory"); 
+		timer = GetNodeOrNull<Timer>("Timer");
+		if (timer == null) {
+			GD.PrintErr("Timer Returned null in Animal Skinning");
+		}
+
 		Skinnable currSkinnable = null;
 		Sheathe = GetNodeOrNull<TextureRect>("Sheathe");
 		CutLine = GetNodeOrNull<Line2D>("CutLine");
+		SkinComment = GetNodeOrNull<RichTextLabel>("SkinCommentBox/SkinComment");
 
 		if (CutLine == null) {
 			GD.Print("Animal Skinning Node: Unable to connect to cutline node");
@@ -50,9 +71,10 @@ public partial class AnimalSkinning : Control
 
 		KnifeAreaNode.MouseOnKnife += (isTrue) => isMouseOnKnife = isTrue; //
 		skinningfact.SkinningInstance += (instance) => setSkinnable(instance); //connects signal from skinnable object to recieve skinnable function.
-		//GetViewport().SizeChanged += setBowieSheathePosition;	 FIXME implement me (pull from old github) I got deleted cause PM fucked up
-		//GetViewport().Ready += setBowieSheathePosition;
 		LineIndex = 0;
+		devAccum = 0;
+
+		timer.Timeout +=  ResetSkinning;
 
 		DefaultBowiePosition = BowieKnife.Position;
 		DefaultSheathePosition = Sheathe.Position;
@@ -72,12 +94,12 @@ public partial class AnimalSkinning : Control
 				double offsetXPosition = DefaultBowiePosition.X - 200.0;
 
 				if (BowieKnife.Position.X != offsetXPosition) {
-					Godot.Vector2 position = BowieKnife.Position;
+					Vector2 position = BowieKnife.Position;
 					
-					BowieKnife.Position = new Godot.Vector2((float)Mathf.Lerp(position.X, offsetXPosition, 5 * delta), DefaultBowiePosition.Y);
+					BowieKnife.Position = new Vector2((float)Mathf.Lerp(position.X, offsetXPosition, 5 * delta), DefaultBowiePosition.Y);
 
 					if (BowieKnife.Position.X - offsetXPosition < 0.1f) {
-						BowieKnife.Position = new Godot.Vector2((float)offsetXPosition, DefaultBowiePosition.Y);
+						BowieKnife.Position = new Vector2((float)offsetXPosition, DefaultBowiePosition.Y);
 					}
 				}
 
@@ -88,8 +110,8 @@ public partial class AnimalSkinning : Control
 			}
 
 			else if (!isMouseOnKnife && BowieKnife.Position != DefaultBowiePosition) {
-				Godot.Vector2 position = BowieKnife.Position; 
-				BowieKnife.Position = new Godot.Vector2((float)Mathf.Lerp(position.X, DefaultBowiePosition.X , 5 * delta), DefaultBowiePosition.Y);
+				Vector2 position = BowieKnife.Position; 
+				BowieKnife.Position = new Vector2((float)Mathf.Lerp(position.X, DefaultBowiePosition.X , 5 * delta), DefaultBowiePosition.Y);
 				if (DefaultBowiePosition.X - BowieKnife.Position.X < 0.1f) {
 						BowieKnife.Position = DefaultBowiePosition;
 					}
@@ -108,7 +130,7 @@ public partial class AnimalSkinning : Control
 			if (Input.IsActionJustPressed("UseItem") && currSkinnable != null && isKnifeOnSkin) {
 				BeginSkinning();
 			}
-		}
+		}	
 
 		else if (!isKnifeHeld && BowieKnife.Position != Sheathe.Position && !isSkinning) {
 			SheatheKnife();
@@ -125,7 +147,7 @@ public partial class AnimalSkinning : Control
 	public void SheatheKnife() {
 		Godot.Vector2 DefaultPosition = Sheathe.Position;
 
-			float lerpFactor = .05f;
+			float lerpFactor = .01f;
 			
 			BowieKnife.Rotation = Mathf.LerpAngle(BowieKnife.Rotation, 0, lerpFactor);
 			BowieKnife.Position = BowieKnife.Position.Lerp(DefaultPosition, lerpFactor);
@@ -142,41 +164,75 @@ public partial class AnimalSkinning : Control
 		}
 	}
 
-	public void Skinning() {			// need to lock bowie knife to skin here too
-		 								// so here I'm saying the total length of the cut line will be 75% of the size of the total collision shape. RectSize.Y is greater(but further down visually) than the location of Y. The difference is the length overall in pixels I beleive.					
+	public void Skinning() {
+												//Accumulates deviation on the x axis relative to the starting point x
+		Godot.Vector2 lastPoint = CutLine.GetPointPosition(LineIndex);
+		float distance = lastPoint.DistanceTo(mousePos);
 
-		float currLength = (float)Math.Sqrt(Math.Pow(mousePos.X - CutLine.GetPointPosition(LineIndex).X, 2) + Math.Pow(mousePos.Y - CutLine.GetPointPosition(LineIndex).Y, 2)); //cur length from the previous point to the mouse pointer.
+		while (distance >= SegmentLength) {						//while the distance between the last point and the mouse is cucked, we split up the line into iterations of proper segment length.
+			float segments = distance / SegmentLength;			//Calculate number of segments we need
+
+			for (int i = 1; i <= segments; i++ ) {				//For each segment we go i/segment length of the way before placing a new point. 							
+				Godot.Vector2 newPoint = lastPoint.Lerp(mousePos, i / segments);
+
+				if (CutLine.GetPointPosition(0).DistanceTo(newPoint) >= MaxLength || LineIndex >= 99) {
+					CutLine.AddPoint(new Godot.Vector2(newPoint.X, CutLine.GetPointPosition(0).Y + MaxLength));
+					RateSkinning(devAccum);
+					return;
+				}
+
+				CutLine.AddPoint(newPoint);
+				devAccum += Mathf.Abs(newPoint.X - currSkinnable.StartMaker.GlobalPosition.X);
+				GD.PrintErr($"AnimalSkinning -- devAccum showing {devAccum}");
+				LineIndex += 1; 
+			}
+
+			lastPoint  = CutLine.GetPointPosition(LineIndex);
+			distance = lastPoint.DistanceTo(mousePos);
+		}
+	}
+
+	public void RateSkinning(float devAccum) 
+	{
+		foreach (KeyValuePair<int, string> kvp in DictRating) {
+			if (devAccum < kvp.Key) {
+				SkinComment.Text  = kvp.Value;
+				timer.Start(2);	//starts timer that once finished will clear skinning node and text.
+				return;
+			}
+		}
 		
-		if (currLength >= SegmentLength) {
-			CutLine.AddPoint(mousePos);
-			LineIndex += 1;					//add another point to the line index counter.
-		}
+		SkinComment.Text  = "Completely Ruined";
+		timer.Start(2);
+	}
 
-		if (LineIndex == 99)				//this will need to be changed if we change the num of segements we want
-		{
-			currSkinnable = null;
-			isSkinning = false;
-			LineIndex = 0;
-		}
+	public void ResetSkinning() 
+	{
+		SkinComment.Text = "";
+		CutLine.ClearPoints();
+		isSkinning = false;
+		LineIndex = 0;
+		devAccum = 0;
+		currSkinnable.QueueFree();
 	}
 
 	public void BeginSkinning() {
 		if (!isSkinning) {
 			currSkinnable.PullRectangle();					
-			Godot.Vector2 rectSize = currSkinnable.GetSize();
-			Godot.Vector2 rectLocation= currSkinnable.GetShapeLocation();
-			Godot.Vector2 offset =  currSkinnable.Position;
-			Godot.Vector2 startPos = currSkinnable.StartMaker.GlobalPosition;
+			Vector2 rectSize = currSkinnable.GetSize();
+			Vector2 rectLocation= currSkinnable.GetShapeLocation();
+			Vector2 offset =  currSkinnable.Position;
+			Vector2 startPos = currSkinnable.StartMaker.GlobalPosition;
 
 			rectLocation += offset;
 			
 			GD.Print($"RectangleSize: {rectSize}\nRectangleLocation: {rectLocation}\nstartPOS: {startPos}");
 
-			totalLength = (rectSize.Y - rectLocation.Y) * .75f; 	// so here I'm saying the total length of the cut line will be 75% of the size of the total collision shape. RectSize.Y is greater(but further down visually) than the location of Y. The difference is the length overall in pixels I beleive.					
-			SegmentLength = totalLength/100;						// so assuming 100 segements will make up our line
+			MaxLength = (rectSize.Y - rectLocation.Y) * .66f; 	// so here I'm saying the total length of the cut line will be 75% of the size of the total collision shape. RectSize.Y is greater(but further down visually) than the location of Y. The difference is the length overall in pixels I beleive.					
+			SegmentLength = MaxLength/100; 						// so assuming 100 segements will make up our line
 
 			BowieKnife.Position = startPos;
-			GetViewport().WarpMouse(startPos);						//FIX ME -- Love this visually. Just need to fix the line drawing during the warp FIXED but I forget how lmao
+			GetViewport().WarpMouse(startPos);						
 			isSkinning = true;
 			CutLine.AddPoint(startPos);
 		}
